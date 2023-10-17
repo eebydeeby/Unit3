54a8c172eaa7bed71d55a355df99d22f0c826f9c
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-10-17 02:03:17 +0100

    Basic gameplay added
    Added scrolling, spawning, and game over state, basic gameplay

--- Assets/MoveLeft.cs
+++ Assets/MoveLeft.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
 	private float speed = 10;
+	private PlayerController playerControllerScript;
+	private float leftBound = -15;
+	
+	void Start() {
+		playerControllerScript =
+		GameObject.Find("Player").GetComponent<PlayerController>();
+	}
 	
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+		if (playerControllerScript.gameOver == false) {
+        	transform.Translate(Vector3.left * Time.deltaTime * speed);
+		}
+		
+		if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle")) {
+			Destroy(gameObject);
+		}
     }
 }
--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 	public GameObject obstaclePrefab;
-	private Vector3 spawnPos = new Vector3(25, 0, 0);
+	private Vector3 spawnPos = new Vector3(25, 2, 0);
 	private float startDelay = 2;
 	private float repeatRate = 2;
+	private PlayerController playerControllerScript;
 	
     void Start() {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+		playerControllerScript =
+		GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
 	void SpawnObstacle () {
-		Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+		if (playerControllerScript.gameOver == false) {
+			Instantiate(obstaclePrefab, spawnPos, Quaternion.identity);
+		}
 	}
 
     // Update is called once per frame
